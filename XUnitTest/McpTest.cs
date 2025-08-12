cc3fd8d762d168f4ccfbacc35c6b4de29e19e2c7
智能大石头 <nnhy@vip.qq.com>
2025-08-12 14:44:54 +0800

    新增MCP模型定义

---
+++ XUnitTest/McpTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NewLife.Data;
+using NewLife.AI;
+using Xunit;
+
+namespace XUnitTest;
+
+public class McpTest
+{
+    [Fact]
+    public async void Tsest()
+    {
+    }
+}
