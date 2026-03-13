750bf46e960b26907611c6c46e7c6779d881a6e1
智能大石头 <nnhy@vip.qq.com>
2026-03-13 14:14:19 +0800

    新增对话设置控制器 ChatSettingController
    新增 ChatSettingController.cs，实现对话设置管理控制器，继承自 ConfigController<ChatSetting>，并配置显示名称、区域标识和菜单属性，支持后台对话设置的管理。

---
+++ NewLife.ChatAI/Areas/ChatAI/Controllers/ChatSettingController.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NewLife.AI.ChatAI;
+using NewLife.ChatAI.Services;
+using NewLife.Cube;
+using NewLife.Cube.Areas.Admin;
+
+namespace NewLife.ChatAI.Areas.ChatAI.Controllers;
+
+/// <summary>对话设置控制器</summary>
+[DisplayName("对话设置")]
+[ChatAIArea]
+[Menu(30, true, Icon = "fa-wrench")]
+public class ChatSettingController : ConfigController<ChatSetting>
+{
+
+}
